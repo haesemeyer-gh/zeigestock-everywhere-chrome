9946c00fb3697a8729f6466acbb708c1802d9fae
haesemeyer <haesemeyer@mailbox.org>
2026-06-24 14:45:25 +0200

    local script
    (does not have local image yet)

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ![Logo: Explodierender Zeigestock](./assets/icon-128.png)
 
-Extension for all Chromium- and Gecko-based Web Browsers that loads the script
-[`https://haesemeyer.dev/scripts/zeigestock.js`](https://codeberg.org/haesemeyer/pages/src/branch/master/scripts/zeigestock.js)
-on every page.
+Extension for all Chromium- and Gecko-based Web Browsers that replaces your cursor with a *Zeigestock* whereever possible.
+Derived from [my websites](https://codeberg.org/haesemeyer/pages) `zeigestock.js`
+*(please consider updating both this extension and the original script with each commit)*
 
-This is extremely stupid, it will download everything all of the time, requires an internet connection and if I replace the script with malicious code then that's your problem now.
+This is extremely stupid, ~~it will download everything all of the time, requires an internet connection and if I replace the script with malicious code then that's your problem now.~~ it runs locally now, but it's still stupid.
 But have you considered how fun having a *Zeigestock* literally ***everywhere*** is!?!?
 
 Licensed under [**CC BY-SA 4.0**](./LICENSE.md)...
---
+++ scripts/zeigestock_ext.js
@@ -0,0 +1,112 @@
+/*
+If you modify this Extension, please also consider modifying the original Script:
+- https://codeberg.org/haesemeyer/pages
+*/
+
+var zeigestockDefaultEnabled = true; // enabled by default with extension
+
+((defaultEnabled) => {
+
+    /* Canvas */
+
+    // Create Canvas
+    let canvas = document.createElement('canvas');
+    canvas.style.position = "fixed";
+    canvas.style.top = 0;
+    canvas.style.left = 0;
+    canvas.style.zIndex = 123450;
+    canvas.style.display = "none";
+    // allow clicking through canvas, disable this so you can't click anything anymore
+    canvas.style.pointerEvents = "none";
+
+    // Canvas is always the size of the viewport
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    // allow resizing window without having to reload page
+    window.addEventListener('resize', () => {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+    });
+
+    document.body.appendChild(canvas);
+
+    // Zeigestock Image Definition
+    let zeigestock = new Image();
+    zeigestock.onload = function() {
+        drawZeigestock(0);
+    };
+    zeigestock.src = "https://haesemeyer.dev/assets/zeigestock.png"; // TODO local copy of zeigestock
+    zeigestock.style.objectFit = "cover";
+
+    let ctx = canvas.getContext('2d');
+
+    // Function to draw Zeigestock on the Canvas
+    function drawZeigestock(angle, mouseX, mouseY) {
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        ctx.save();
+        ctx.translate(window.innerWidth, window.innerHeight);
+        ctx.rotate((Math.PI/2)+0.02);
+        ctx.rotate(angle);
+        zeigestock.height = Math.sqrt( Math.pow((mouseY), 2) + Math.pow((mouseX), 2) );
+        ctx.drawImage(zeigestock, -zeigestock.width, -zeigestock.height, );
+        ctx.restore();
+    }
+    
+    // Function to draw Zeigestock at Mouse Position
+    function moveMouse(x, y) {
+        let dx = x - window.innerWidth;
+        let dy = y - window.innerHeight;
+        let theta = Math.atan2(dy, dx);
+        drawZeigestock(theta, dx, dy);
+    }
+
+    // Mouse Move Event
+    window.addEventListener('pointermove', (e) => {
+        moveMouse(e.clientX, e.clientY);
+    });
+    // Touch Screen Event
+    window.addEventListener('touchmove', (e) => {
+        moveMouse(e.touches[0].clientX, e.touches[0].clientY);
+        e.preventDefault();
+    }, { passive: false }); // passive: false on extension for better performance, disables scrolling on actual page script
+
+    /* Menu */
+
+    let zeigestockToggleText = document.createElement('span');
+    zeigestockToggleText.innerText = "Zeigestock: ";
+
+    let zeigestockToggleBox = document.createElement('input');
+    zeigestockToggleBox.type = "checkbox";
+    zeigestockToggleBox.addEventListener('input', () => {
+        // Toggle Canvas Visibility
+        canvas.style.display = (zeigestockToggleBox.checked ? "block" : "none");
+        // Toggle Hidden Cursor
+        if (zeigestockToggleBox.checked) {
+            let hiddenCursor = document.createElement('style');
+            hiddenCursor.id = 'zeigestock-hidden-cursor-style';
+            hiddenCursor.textContent = "* { cursor: none !important; }";
+            document.head.appendChild(hiddenCursor);
+        } else {
+            document.getElementById('zeigestock-hidden-cursor-style').remove();
+        }
+    });
+
+    let zeigestockToggle = document.createElement('label');
+    zeigestockToggle.append(zeigestockToggleText, zeigestockToggleBox);
+
+    let zeigestockMenu = document.createElement('div');
+    zeigestockMenu.ariaHidden = true;
+    zeigestockMenu.style.position = "fixed";
+    zeigestockMenu.style.bottom = 0;
+    zeigestockMenu.style.right = 0;
+    zeigestockMenu.style.zIndex = 123451;
+    zeigestockMenu.appendChild(zeigestockToggle);
+    
+    document.body.appendChild(zeigestockMenu);
+
+    if (defaultEnabled) {
+        zeigestockToggleBox.checked = true;
+        zeigestockToggleBox.dispatchEvent(new Event('input'));
+    }
+
+})(zeigestockDefaultEnabled);
