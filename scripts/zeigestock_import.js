8ee31e77d771b7d16a8f601aff131b0bb89fab89
haesemeyer <haesemeyer@mailbox.org>
2026-06-24 11:14:52 +0200

    init

---
+++ scripts/zeigestock_import.js
@@ -0,0 +1,7 @@
+var zeigestockDefaultEnabled = true;
+
+(() => {
+    let s = document.createElement('script');
+    s.src="https://haesemeyer.dev/scripts/zeigestock.js";
+    document.head.appendChild(s);
+})();
